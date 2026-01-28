04d7a7c1311e7ccd7818917b3f522f7f2beda0b0
Sotiris Lamprinidis <sot.lampr@gmail.com>
2026-01-28 12:17:49 +0100

    Update run.sh model hyper-parameters

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-# These variables can be overrided in the calling environment
-DATA_PATH=${DATA_PATH:=../data}
+# These variables can be overriden in the calling environment
+DATA_PATH=${DATA_PATH:=../../seg_root_in_soil_next/data}
 DATASETS=${DATASETS:=chicory cotton grassland papaya peanut sesame sunflower switchgrass deeproot_ann}
 NUM_RUNS=${NUM_RUNS:=2}
 OUTDIR=${OUTDIR:=../results}
 NUM_WORKERS=${NUM_WORKERS:=20}
+PRETRAINED=${PRETRAINED:=false true}
+LEARNING_RATE=${LEARNING_RATE:=1e-3}
 
-# The models to run can be overrided by passing positional arguments
+# The models to run can be overriden by passing positional arguments
 TODO="\
   m2f/R50 \
   m2f/swin-small \
@@ -45,104 +47,96 @@ trap 'die $LINENO' ERR
 batch_size () {
   run_id=$1
   case $run_id in
+    segmentation_pytorch/segformer-mit_b1-chicory-true|\
     segmentation_pytorch/segformer-mit_b1-deeproot_ann-true|\
+    segmentation_pytorch/segformer-mit_b1-grassland-true|\
+    segmentation_pytorch/segformer-mit_b2-chicory-true|\
     segmentation_pytorch/segformer-mit_b2-deeproot_ann-true|\
-    segmentation_pytorch/segformer-mit_b3-deeproot_ann-*)
+    segmentation_pytorch/segformer-mit_b2-grassland-true|\
+    segmentation_pytorch/segformer-mit_b3-chicory-*|\
+    segmentation_pytorch/segformer-mit_b3-deeproot_ann-*|\
+    segmentation_pytorch/segformer-mit_b3-grassland-*)
       echo 2;;
+    m2f/R50-chicory-*|\
     m2f/R50-deeproot_ann-*|\
+    m2f/R50-grassland-*|\
+    m2f/swin-small-chicory-*|\
     m2f/swin-small-deeproot_ann-*|\
+    m2f/swin-small-grassland-*|\
+    m2f/swin-tiny-chicory-*|\
     m2f/swin-tiny-deeproot_ann-*|\
+    m2f/swin-tiny-grassland-*|\
+    rootnav/hourglass-chicory-*|\
+    samII/hiera-base_plus-chicory-*|\
     samII/hiera-base_plus-cotton-*|\
-    samII/hiera-base_plus-papaya-*|\
-    segmentation_pytorch/segformer-mit_b1-deeproot_ann-false|\
-    segmentation_pytorch/segformer-mit_b2-deeproot_ann-false|\
-    samII/hiera-base_plus-sesame-*|\
     samII/hiera-base_plus-deeproot_ann-*|\
-    samII/hiera-base_plus-switchgrass-*)
+    samII/hiera-base_plus-grassland-*|\
+    samII/hiera-base_plus-papaya-*|\
+    samII/hiera-base_plus-peanut-*|\
+    samII/hiera-base_plus-sesame-*|\
+    samII/hiera-base_plus-sunflower-*|\
+    samII/hiera-base_plus-switchgrass-*|\
+    segmentation_pytorch/segformer-mit_b1-chicory-false|\
+    segmentation_pytorch/segformer-mit_b1-deeproot_ann-false|\
+    segmentation_pytorch/segformer-mit_b1-grassland-false|\
+    segmentation_pytorch/segformer-mit_b2-chicory-false|\
+    segmentation_pytorch/segformer-mit_b2-deeproot_ann-false|\
+    segmentation_pytorch/segformer-mit_b2-grassland-false)
       echo 4;;
     m2f/swin-small-cotton-*|\
     m2f/swin-small-papaya-*|\
+    m2f/swin-small-peanut-*|\
     m2f/swin-small-sesame-*|\
+    m2f/swin-small-sunflower-*|\
     m2f/swin-small-switchgrass-*|\
     m2f/swin-tiny-cotton-*|\
     m2f/swin-tiny-papaya-*|\
+    m2f/swin-tiny-peanut-*|\
     m2f/swin-tiny-sesame-*|\
     m2f/swin-tiny-switchgrass-*|\
     mb_sam/vit_t-cotton-*|\
+    mb_sam/vit_t-grassland-false|\
     rootnav/hourglass-cotton-*|\
     rootnav/hourglass-deeproot_ann-*|\
+    rootnav/hourglass-grassland-*|\
     rootnav/hourglass-papaya-*|\
+    rootnav/hourglass-peanut-*|\
     rootnav/hourglass-sesame-*|\
+    rootnav/hourglass-sunflower-*|\
     rootnav/hourglass-switchgrass-*|\
     samII/hiera-base_plus-sesame-false|\
+    samII/hiera-small-chicory-*|\
     samII/hiera-small-cotton-*|\
     samII/hiera-small-deeproot_ann-*|\
+    samII/hiera-small-grassland-*|\
     samII/hiera-small-papaya-*|\
+    samII/hiera-small-peanut-*|\
     samII/hiera-small-sesame-*|\
+    samII/hiera-small-sunflower-*|\
     samII/hiera-small-switchgrass-*|\
     segmentation_pytorch/segformer-mit_b1-cotton-*|\
     segmentation_pytorch/segformer-mit_b1-papaya-*|\
+    segmentation_pytorch/segformer-mit_b1-peanut-true|\
     segmentation_pytorch/segformer-mit_b1-sesame-*|\
+    segmentation_pytorch/segformer-mit_b1-sunflower-true|\
     segmentation_pytorch/segformer-mit_b1-switchgrass-*|\
     segmentation_pytorch/segformer-mit_b2-cotton-*|\
     segmentation_pytorch/segformer-mit_b2-papaya-*|\
+    segmentation_pytorch/segformer-mit_b2-peanut-*|\
     segmentation_pytorch/segformer-mit_b2-sesame-*|\
+    segmentation_pytorch/segformer-mit_b2-sunflower-true|\
     segmentation_pytorch/segformer-mit_b2-switchgrass-*|\
     segmentation_pytorch/segformer-mit_b3-cotton-*|\
     segmentation_pytorch/segformer-mit_b3-papaya-*|\
+    segmentation_pytorch/segformer-mit_b3-peanut-*|\
     segmentation_pytorch/segformer-mit_b3-sesame-*|\
+    segmentation_pytorch/segformer-mit_b3-sunflower-*|\
     segmentation_pytorch/segformer-mit_b3-switchgrass-*)
       echo 8;;
     *)
       echo 16;;
     esac
-}
 
-learning_rate () {
-  run_id=$1
-  case $run_id in
-    m2f/R50-deeproot_ann-*|\
-    m2f/swin-small-deeproot_ann-*|\
-    m2f/swin-tiny-deeproot_ann-*|\
-    samII/hiera-base_plus-cotton-false|\
-    samII/hiera-base_plus-deeproot_ann-*|\
-    samII/hiera-base_plus-sesame-false|\
-    samII/hiera-small-cotton-false|\
-    samII/hiera-small-deeproot_ann-*|\
-    samII/hiera-small-sesame-false|\
-    segmentation_pytorch/manet-inceptionv4-deeproot_ann-false|\
-    segmentation_pytorch/manet-resnet50-deeproot_ann-false|\
-    segroot/w8d5-deeproot_ann-false|\
-    unet_valid/GN-deeproot_ann-false|\
-    unet_valid/GNRes-deeproot_ann-false)
-      echo "nan";;
-    mb_sam/vit_t-cotton-false|\
-    segroot-w64d4-cotton-*)
-      echo 1e-2;;
-    m2f/R50-deeproot_ann-true|\
-    m2f/swin-small-deeproot_ann-true|\
-    m2f/swin-tiny-deeproot_ann-true|\
-    mb_sam/vit_t-deeproot_ann-false|\
-    rootnav/hourglass-deeproot_ann-false|\
-    samII/hiera-base_plus-deeproot_ann-true|\
-    samII/hiera-base_plus-switchgrass-false|\
-    samII/hiera-small-deeproot_ann-true|\
-    segmentation_pytorch/deeplabv3+-resnet50-deeproot_ann-false|\
-    segmentation_pytorch/linknet-inceptionv4-deeproot_ann-false|\
-    segmentation_pytorch/manet-resnet50-deeproot_ann-true|\
-    segmentation_pytorch/segformer-mit_b1-deeproot_ann-*|\
-    segmentation_pytorch/segformer-mit_b2-deeproot_ann-*|\
-    segmentation_pytorch/segformer-mit_b3-deeproot_ann-*|\
-    segmentation_pytorch/unet++-inceptionv4-deeproot_ann-false|\
-    segmentation_pytorch/unet++-resnet50-deeproot_ann-false|\
-    segroot/w16d4-deeproot_ann-false|\
-    segroot/w32d5-deeproot_ann-false|\
-    segroot/w64d4-deeproot_ann-false|\
-    torchvision_models/deeplabv3-resnet50-deeproot_ann-false)
-      echo 1e-4;;
-    *)
-      echo 1e-3;;
-    esac
 }
 
 shape () {
@@ -173,7 +167,7 @@ for i in $(seq 1 $NUM_RUNS); do
   for model in $TODO; do
     model_name=$(echo $model| tr / -)
     for dataset in $DATASETS; do
-      for pretrain in false true; do
+      for pretrain in $PRETRAINED; do
         out_dir="$OUTDIR/$model_name-$dataset-$i"
 
         # handle pretrained case - add appropriate flag
@@ -191,7 +185,7 @@ for i in $(seq 1 $NUM_RUNS); do
         fi
 
         bs=$(batch_size $model-$dataset-$pretrain)
-        lr=$(learning_rate $model-$dataset-$pretrain)
+        lr=$LEARNING_RATE
         if test $lr = "nan"; then continue; fi
         s=$(shape $model)
           
