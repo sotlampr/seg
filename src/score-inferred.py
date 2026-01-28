7b1741e644f07af7cd8124d6dbc83985e7c811c7
Sotiris Lamprinidis <sot.lampr@gmail.com>
2026-01-28 09:45:18 +0100

    Modify gather-results to calculate dice on corrective annotations

--- src/score-inferred.py
+++ src/score-inferred.py
@@ -10,10 +10,10 @@ from torchvision.io import read_image as read_image_, ImageReadMode
 
 from seg import f1_score_, tp_fp_fn
 
-
-def read_image(fn):
-    img = read_image_(fn, ImageReadMode.GRAY)
-    return img.cuda()
+def read_image(fn, mode="GRAY"):
+    img = read_image_(fn, getattr(ImageReadMode, mode))
+    #return img.cuda()
+    return img
 
 
 if len(sys.argv) != 2 or sys.argv[1] in {"-h", "--help"}:
@@ -22,9 +22,15 @@ if len(sys.argv) != 2 or sys.argv[1] in {"-h", "--help"}:
         "where BASE_PATH contains annotations/*.png and any model folders.")
     sys.exit(1)
 
+
+sparse_annotations = False
+
+if "deeproot_ann" in sys.argv[1]:
+    sparse_annotations = True
+
 base_path = sys.argv[1]
 target_imgs = {
-    os.path.basename(fn): read_image(fn)
+    os.path.basename(fn): read_image(fn, "RGB" if sparse_annotations else "GRAY")
     for fn in glob.glob(f"{base_path}/annotations/*.png")
 }
 
@@ -33,16 +39,25 @@ for model_path in glob.glob(f"{base_path}/*"):
     if model_path.endswith("annotations") or model_path.endswith("photos"):
         continue
 
-    # tps fps fns expects logits, so we make sure 0s are negative
     model_imgs = {
         fn: read_image(f"{model_path}/{fn}")
         for fn in target_imgs
     }
 
-    tps_fps_fns = {
-        fn: tp_fp_fn(model_imgs[fn]>0, target_imgs[fn])
-        for fn in target_imgs
-    }
+    if sparse_annotations:
+        tps_fps_fns = {}
+        for fn in target_imgs:
+            # either red or green
+            mask = target_imgs[fn][:2].any(0).bool()
+            # red: positive class
+            targ = target_imgs[fn][0]
+            tps_fps_fns[fn] = tp_fp_fn(model_imgs[fn][0][mask]>0, targ[mask]>0)
+    else:
+        # tps fps fns expects logits, so we make sure 0s are negative
+        tps_fps_fns = {
+            fn: tp_fp_fn(model_imgs[fn]>0, target_imgs[fn])
+            for fn in target_imgs
+        }
 
     tps, fps, fns = torch.tensor(list(tps_fps_fns.values())).sum(0)
 
