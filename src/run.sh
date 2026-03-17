b1055e62cc8ce07034d1bccb6b99a3030a587701
Sotiris Lamprinidis <sot.lampr@gmail.com>
2026-03-17 18:47:15 +0100

    Script to run the ablation study

--- src/run.sh
+++ src/run.sh
@@ -13,39 +13,22 @@
 #   ./run,sh m2f/R50 m2f/swin-small
 set -e
 
-# These variables can be overriden in the calling environment
-DATA_PATH=${DATA_PATH:=../../seg_root_in_soil_next/data}
-DATASETS=${DATASETS:=chicory cotton grassland papaya peanut sesame sunflower switchgrass deeproot_ann}
+# These variables can be overrided in the calling environment
+DATA_PATH=${DATA_PATH:=$HOME/src/seg/data}
+if test ${HOSTNAME} = "triton"; then
+  DATASETS="chicory grassland peanut sunflower"
+elif test ${HOSTNAME} = "prime"; then
+ DATASETS="cotton deeproot_ann papaya sesame switchgrass"
+else
+  echo "Unkown host '$HOSTNAME'"
+  exit 2
+fi
 NUM_RUNS=${NUM_RUNS:=2}
-OUTDIR=${OUTDIR:=../results}
+OUTDIR=${OUTDIR:=../results-ablation-patch-size}
 NUM_WORKERS=${NUM_WORKERS:=20}
-PRETRAINED=${PRETRAINED:=false true}
-LEARNING_RATE=${LEARNING_RATE:=1e-3}
+LEARNING_RATE=${LEARNING_RATE:=1e-4}
 
-# The models to run can be overriden by passing positional arguments
-TODO="\
-  m2f/R50 \
-  m2f/swin-small \
-  m2f/swin-tiny \
-  mb_sam/vit_t \
-  samII/hiera-small \
-  samII/hiera-base_plus \
-  rootnav/hourglass \
-  segmentation_pytorch/deeplabv3+-resnet50 \
-  segmentation_pytorch/linknet-inceptionv4 \
-  segmentation_pytorch/linknet-resnet50 \
-  segmentation_pytorch/manet-inceptionv4 \
-  segmentation_pytorch/manet-resnet50 \
-  segmentation_pytorch/segformer-mit_b1 \
-  segmentation_pytorch/segformer-mit_b2 \
-  segmentation_pytorch/segformer-mit_b3 \
-  segmentation_pytorch/unet++-inceptionv4 \
-  segmentation_pytorch/unet++-resnet50 \
-  segroot/w8d5 \
-  torchvision_models/deeplabv3-resnet50 \
-  unet_valid/GN \
-  unet_valid/GNRes \
-"
+TODO="mb_sam/vit_t segmentation_pytorch/manet-inceptionv4"
 
 die() {
     echo "Error on line $1"
@@ -59,91 +42,7 @@ trap 'die $LINENO' ERR
 batch_size () {
   run_id=$1
   case $run_id in
-    segmentation_pytorch/segformer-mit_b1-chicory-true|\
-    segmentation_pytorch/segformer-mit_b1-deeproot_ann-true|\
-    segmentation_pytorch/segformer-mit_b1-grassland-true|\
-    segmentation_pytorch/segformer-mit_b2-chicory-true|\
-    segmentation_pytorch/segformer-mit_b2-deeproot_ann-true|\
-    segmentation_pytorch/segformer-mit_b2-grassland-true|\
-    segmentation_pytorch/segformer-mit_b3-chicory-*|\
-    segmentation_pytorch/segformer-mit_b3-deeproot_ann-*|\
-    segmentation_pytorch/segformer-mit_b3-grassland-*)
-      echo 2;;
-    m2f/R50-chicory-*|\
-    m2f/R50-deeproot_ann-*|\
-    m2f/R50-grassland-*|\
-    m2f/swin-small-chicory-*|\
-    m2f/swin-small-deeproot_ann-*|\
-    m2f/swin-small-grassland-*|\
-    m2f/swin-tiny-chicory-*|\
-    m2f/swin-tiny-deeproot_ann-*|\
-    m2f/swin-tiny-grassland-*|\
-    rootnav/hourglass-chicory-*|\
-    samII/hiera-base_plus-chicory-*|\
-    samII/hiera-base_plus-cotton-*|\
-    samII/hiera-base_plus-deeproot_ann-*|\
-    samII/hiera-base_plus-grassland-*|\
-    samII/hiera-base_plus-papaya-*|\
-    samII/hiera-base_plus-peanut-*|\
-    samII/hiera-base_plus-sesame-*|\
-    samII/hiera-base_plus-sunflower-*|\
-    samII/hiera-base_plus-switchgrass-*|\
-    segmentation_pytorch/segformer-mit_b1-chicory-false|\
-    segmentation_pytorch/segformer-mit_b1-deeproot_ann-false|\
-    segmentation_pytorch/segformer-mit_b1-grassland-false|\
-    segmentation_pytorch/segformer-mit_b2-chicory-false|\
-    segmentation_pytorch/segformer-mit_b2-deeproot_ann-false|\
-    segmentation_pytorch/segformer-mit_b2-grassland-false)
-      echo 4;;
-    m2f/swin-small-cotton-*|\
-    m2f/swin-small-papaya-*|\
-    m2f/swin-small-peanut-*|\
-    m2f/swin-small-sesame-*|\
-    m2f/swin-small-sunflower-*|\
-    m2f/swin-small-switchgrass-*|\
-    m2f/swin-tiny-cotton-*|\
-    m2f/swin-tiny-papaya-*|\
-    m2f/swin-tiny-peanut-*|\
-    m2f/swin-tiny-sesame-*|\
-    m2f/swin-tiny-switchgrass-*|\
-    mb_sam/vit_t-cotton-*|\
-    mb_sam/vit_t-grassland-false|\
-    rootnav/hourglass-cotton-*|\
-    rootnav/hourglass-deeproot_ann-*|\
-    rootnav/hourglass-grassland-*|\
-    rootnav/hourglass-papaya-*|\
-    rootnav/hourglass-peanut-*|\
-    rootnav/hourglass-sesame-*|\
-    rootnav/hourglass-sunflower-*|\
-    rootnav/hourglass-switchgrass-*|\
-    samII/hiera-base_plus-sesame-false|\
-    samII/hiera-small-chicory-*|\
-    samII/hiera-small-cotton-*|\
-    samII/hiera-small-deeproot_ann-*|\
-    samII/hiera-small-grassland-*|\
-    samII/hiera-small-papaya-*|\
-    samII/hiera-small-peanut-*|\
-    samII/hiera-small-sesame-*|\
-    samII/hiera-small-sunflower-*|\
-    samII/hiera-small-switchgrass-*|\
-    segmentation_pytorch/segformer-mit_b1-cotton-*|\
-    segmentation_pytorch/segformer-mit_b1-papaya-*|\
-    segmentation_pytorch/segformer-mit_b1-peanut-true|\
-    segmentation_pytorch/segformer-mit_b1-sesame-*|\
-    segmentation_pytorch/segformer-mit_b1-sunflower-true|\
-    segmentation_pytorch/segformer-mit_b1-switchgrass-*|\
-    segmentation_pytorch/segformer-mit_b2-cotton-*|\
-    segmentation_pytorch/segformer-mit_b2-papaya-*|\
-    segmentation_pytorch/segformer-mit_b2-peanut-*|\
-    segmentation_pytorch/segformer-mit_b2-sesame-*|\
-    segmentation_pytorch/segformer-mit_b2-sunflower-true|\
-    segmentation_pytorch/segformer-mit_b2-switchgrass-*|\
-    segmentation_pytorch/segformer-mit_b3-cotton-*|\
-    segmentation_pytorch/segformer-mit_b3-papaya-*|\
-    segmentation_pytorch/segformer-mit_b3-peanut-*|\
-    segmentation_pytorch/segformer-mit_b3-sesame-*|\
-    segmentation_pytorch/segformer-mit_b3-sunflower-*|\
-    segmentation_pytorch/segformer-mit_b3-switchgrass-*)
+    mb_sam/vit_t-cotton-*)
       echo 8;;
     *)
       echo 16;;
@@ -154,11 +53,10 @@ shape () {
   model=$1
   case $model in
     m2f*|mb_sam*|sam*|segmentation_pytorch/segformer*)
-      echo 1024 1024;;
-    segmentation_pytorch/*|rootnav*|segroot*)
       echo 576 576;;
+    segmentation_pytorch/*|rootnav*|segroot*)
+      echo 1024 1024;;
     *)
-      echo 572 572;;
   esac
 }
 
@@ -178,7 +76,7 @@ for i in $(seq 1 $NUM_RUNS); do
   for model in $TODO; do
     model_name=$(echo $model| tr / -)
     for dataset in $DATASETS; do
-      for pretrain in $PRETRAINED; do
+      for pretrain in false true; do
         out_dir="$OUTDIR/$model_name-$dataset-$i"
 
         # handle pretrained case - add appropriate flag
@@ -220,7 +118,7 @@ for i in $(seq 1 $NUM_RUNS); do
         echo "	ARGS           = $extra_args"
         extra_args="$extra_args"
 
-        ./seg.py $DATA_PATH/$dataset $model -o$out_dir $workers_flag $extra_args || echo $model-$dataset-$pretrain >> failed
+        seg $model $dataset -o$out_dir $workers_flag $extra_args || echo $model-$dataset-$pretrain >> failed
       done
     done
   done
